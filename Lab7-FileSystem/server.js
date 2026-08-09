f9703e5646048c5aa4614a7049ba1e5d813b25f9
Varsha <varshasanjay399@gmail.com>
2026-08-09 12:42:27 +0530

    Add Lab 7 File System CRUD

---
+++ Lab7-FileSystem/server.js
@@ -0,0 +1,227 @@
+const express = require("express");
+const fs = require("fs").promises;
+const path = require("path");
+
+const app = express();
+
+const PORT = 4000;
+
+const dataFile = path.join(
+    __dirname,
+    "data",
+    "cafes.json"
+);
+
+// MIDDLEWARE
+app.use(express.json());
+
+
+// Serve frontend
+app.use(express.static(
+    path.join(__dirname, "client")
+));
+
+
+// HELPER FUNCTIONS
+
+
+// Read cafes from JSON file
+
+async function readCafes() {
+
+    const data = await fs.readFile(
+        dataFile,
+        "utf-8"
+    );
+
+    return JSON.parse(data);
+
+}
+
+
+// Write cafes to JSON file
+
+async function writeCafes(cafes) {
+
+    await fs.writeFile(
+        dataFile,
+        JSON.stringify(cafes, null, 2)
+    );
+
+}
+
+
+// GET - READ
+
+
+app.get("/api/file-cafes", async (req, res) => {
+
+    try {
+
+        const cafes = await readCafes();
+
+        res.json(cafes);
+
+    }
+
+    catch (err) {
+
+        res.status(500).json({
+            message: err.message
+        });
+
+    }
+
+});
+
+
+// POST - CREATE
+
+app.post("/api/file-cafes", async (req, res) => {
+
+    try {
+
+        const cafes = await readCafes();
+
+        const newCafe = {
+
+            id: Date.now(),
+
+            name: req.body.name,
+
+            location: req.body.location,
+
+            event: req.body.event,
+
+            rating: req.body.rating || 4.5
+
+        };
+
+        cafes.push(newCafe);
+
+        await writeCafes(cafes);
+
+        res.status(201).json(newCafe);
+
+    }
+
+    catch (err) {
+
+        res.status(500).json({
+            message: err.message
+        });
+
+    }
+
+});
+
+// PUT - UPDATE
+
+
+app.put("/api/file-cafes/:id", async (req, res) => {
+
+    try {
+
+        const cafes = await readCafes();
+
+        const id = Number(req.params.id);
+
+        const index = cafes.findIndex(
+            cafe => cafe.id === id
+        );
+
+
+        if (index === -1) {
+
+            return res.status(404).json({
+                message: "Cafe not found"
+            });
+
+        }
+
+
+        cafes[index] = {
+
+            ...cafes[index],
+
+            name: req.body.name,
+
+            location: req.body.location,
+
+            event: req.body.event,
+
+            rating: req.body.rating || 4.5
+
+        };
+
+
+        await writeCafes(cafes);
+
+        res.json(cafes[index]);
+
+    }
+
+    catch (err) {
+
+        res.status(500).json({
+            message: err.message
+        });
+
+    }
+
+});
+
+
+// DELETE
+
+
+app.delete("/api/file-cafes/:id", async (req, res) => {
+
+    try {
+
+        const cafes = await readCafes();
+
+        const id = Number(req.params.id);
+
+        const filteredCafes = cafes.filter(
+            cafe => cafe.id !== id
+        );
+
+
+        if (filteredCafes.length === cafes.length) {
+
+            return res.status(404).json({
+                message: "Cafe not found"
+            });
+
+        }
+
+
+        await writeCafes(filteredCafes);
+
+        res.json({
+            message: "Cafe deleted successfully"
+        });
+
+    }
+
+    catch (err) {
+
+        res.status(500).json({
+            message: err.message
+        });
+
+    }
+
+});
+
+// START SERVER
+
+
+app.listen(PORT, () => {
+
+    console.log(
+        `Lab 7 server running at http://localhost:${PORT}`
+    );
+
+});
